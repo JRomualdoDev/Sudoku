c0854ca0941174d7bb04dc0d93d4b3aba575e755
JoseRomualdo <jromualdo3@hotmail.com>
2025-07-28 15:22:48 -0300

    Grafics complete

---
+++ src/main/java/org/dio/sudoku/model/service/EventEnum.java
@@ -0,0 +1,7 @@
+package org.dio.sudoku.model.service;
+
+public enum EventEnum {
+
+    CLEAR_SPACE
+
+}
---
+++ src/main/java/org/dio/sudoku/model/service/EventListener.java
@@ -0,0 +1,5 @@
+package org.dio.sudoku.model.service;
+
+public interface EventListener {
+    void update(final EventEnum eventType);
+}
